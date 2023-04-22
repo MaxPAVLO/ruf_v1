181392a3aff4aca2c5cfafe0d4fbb0d179bf6a09
Максим Павлов Вадимович <104314341+MaxPAVLO@users.noreply.github.com>
2023-04-22 17:29:36 +0300

    3
    Вся моя часть и часть Вани

---
+++ final_win.py
@@ -0,0 +1,32 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout
+from instr import *
+app = QApplication([])
+
+class FinalWin(QWidget):
+    def set_appear(self):
+        #Заголовок, место и размеры окна
+        self.resize(win_width, win_height)
+        self.setWindowTitle("Тест Руфье")
+        self.move(win_X, win_y)
+
+    def initUI(self):
+        #Создание главного лайаута, индекс Руфье, итог
+        self.mainLayout = QVBoxLayout()
+        self.indexLabel = QLabel(index)
+        self.resultLabel = QLabel(result)
+
+        #Добавление виджетов на лайаут
+        self.mainLayout.addWidget(self.indexLabel, alignment = Qt.AlignCenter)
+        self.mainLayout.addWidget(self.resultLabel, alignment = Qt.AlignCenter)
+
+        #Позиционирование главного лайаута на экране
+        self.setLayout(self.mainLayout)
+
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.show()
+
+app.exec_()
